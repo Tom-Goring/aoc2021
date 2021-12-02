38c1dbced6195b2d503b4e93942a541ebdb978b4
Tom Goring <mail@tomgoring.co.uk>
2021-12-02 19:57:12 +0000

    D2 p2

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,27 @@ use itertools::Itertools;
 use std::fs;
 
 fn main() {
-    let count = fs::read_to_string("resources/day1.txt")
+    let count = fs::read_to_string("resources/day2.txt")
         .unwrap()
         .lines()
-        .map(|l| l.parse::<i32>().unwrap())
-        .tuple_windows::<(i32, i32, i32)>()
-        .tuple_windows()
-        .filter(|((a, b, c), (x, y, z))| a + b + c < x + y + z)
-        .count();
+        .map(|l| {
+            l.split_whitespace()
+                .collect_tuple::<(&str, &str)>()
+                .unwrap()
+        })
+        .map(|command| (command.0, str::parse(command.1).unwrap()))
+        .fold(
+            (0, 0),
+            |(horizontal_depth, vertical_depth), (direction, magnitude): (&str, i32)| {
+                match direction {
+                    "forward" => (horizontal_depth + magnitude, vertical_depth),
+                    "up" => (horizontal_depth, vertical_depth - magnitude),
+                    "down" => (horizontal_depth, vertical_depth + magnitude),
+                    _ => panic!("Unexpected direction"),
+                }
+            },
+        );
 
-    println!("{}", count);
+    println!("{:?}", count);
+    println!("{}", count.0 * count.1);
 }
