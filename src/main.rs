7ccc8c91c55d2147fd54c4790a8c2483015a9742
Tom Goring <mail@tomgoring.co.uk>
2021-12-03 17:58:34 +0000

    D3 p1

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,78 @@
-use itertools::Itertools;
+/*
+--- Day 3: Binary Diagnostic ---
+The submarine has been making some odd creaking noises, so you ask it to produce a diagnostic report just in case.
+
+The diagnostic report (your puzzle input) consists of a list of binary numbers which, when decoded properly, can
+tell you many useful things about the conditions of the submarine. The first parameter to check is the power consumption.
+
+You need to use the binary numbers in the diagnostic report to generate two new binary numbers (called the gamma rate and
+the epsilon rate). The power consumption can then be found by multiplying the gamma rate by the epsilon rate.
+
+Each bit in the gamma rate can be determined by finding the most common bit in the corresponding position of all numbers
+in the diagnostic report. For example, given the following diagnostic report:
+
+00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010
+
+Considering only the first bit of each number, there are five 0 bits and seven 1 bits. Since the most common bit is 1, the first bit of the gamma rate is 1.
+
+The most common second bit of the numbers in the diagnostic report is 0, so the second bit of the gamma rate is 0.
+
+The most common value of the third, fourth, and fifth bits are 1, 1, and 0, respectively, and so the final three bits of the gamma rate are 110.
+
+So, the gamma rate is the binary number 10110, or 22 in decimal.
+
+The epsilon rate is calculated in a similar way; rather than use the most common bit, the least common bit from each position is used. So, the epsilon rate is 01001, or 9 in decimal.
+Multiplying the gamma rate (22) by the epsilon rate (9) produces the power consumption, 198.
+
+Use the binary numbers in your diagnostic report to calculate the gamma rate and epsilon rate, then multiply them together. What is the power consumption of the submarine?
+(Be sure to represent your answer in decimal, not binary.)
+*/
+
 use std::fs;
 
 fn main() {
-    let count = fs::read_to_string("resources/day2.txt")
-        .unwrap()
-        .lines()
-        .map(|l| {
-            l.split_whitespace()
-                .collect_tuple::<(&str, &str)>()
-                .unwrap()
-        })
-        .map(|command| (command.0, str::parse(command.1).unwrap()))
-        .fold(
-            (0, 0, 0),
-            |(horizontal_position, vertical_depth, aim), (direction, magnitude): (&str, i32)| {
-                match direction {
-                    "forward" => (
-                        horizontal_position + magnitude,
-                        vertical_depth + aim * magnitude,
-                        aim,
-                    ),
-                    "up" => (horizontal_position, vertical_depth, aim - magnitude),
-                    "down" => (horizontal_position, vertical_depth, aim + magnitude),
-                    _ => panic!("Unexpected direction"),
-                }
-            },
-        );
+    let mut zeros = [0; 12];
+    let mut ones = [0; 12];
+    for line in fs::read_to_string("resources/day3.txt").unwrap().lines() {
+        for (idx, char) in line.chars().enumerate() {
+            match char {
+                '0' => zeros[idx] += 1,
+                '1' => ones[idx] += 1,
+                _ => unimplemented!(),
+            }
+        }
+    }
 
-    println!("{:?}", count);
-    println!("{}", count.0 * count.1);
+    let gamma = i32::from_str_radix(
+        &zeros
+            .iter()
+            .zip(ones.iter())
+            .map(|(x, y)| if x > y { '0' } else { '1' })
+            .collect::<String>(),
+        2,
+    )
+    .unwrap();
+
+    let epsilon = i32::from_str_radix(
+        &zeros
+            .iter()
+            .zip(ones.iter())
+            .map(|(x, y)| if y > x { '0' } else { '1' })
+            .collect::<String>(),
+        2,
+    )
+    .unwrap();
+
+    println!("{}", gamma * epsilon);
 }
