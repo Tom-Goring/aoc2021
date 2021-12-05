66ce94fe0d126190cf26eb9c1bb443eab857b430
Tom Goring <mail@tomgoring.co.uk>
2021-12-05 13:22:21 +0000

    D5 p1

--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,55 @@
-use std::fs;
+use itertools::Itertools;
+use parse_display::{Display, FromStr};
+use std::{fs, str::FromStr};
 
-#[derive(Debug, Clone)]
-struct Board {
-    rows: Vec<Vec<i32>>,
-    cols: Vec<Vec<i32>>,
+#[derive(Display, FromStr, Debug, Copy, Clone)]
+#[display("{x},{y}")]
+struct Point {
+    x: i32,
+    y: i32,
 }
 
-impl Board {
-    pub fn new(rows: Vec<Vec<i32>>) -> Self {
-        let cols: Vec<Vec<i32>> = (0..rows.len())
-            .map(|i| rows.iter().map(|c| c[i]).collect())
-            .collect();
-        Board { rows, cols }
-    }
-
-    pub fn check_for_bingo(&self, winning_numbers: &[i32]) -> bool {
-        self.rows
-            .iter()
-            .any(|row| row.iter().all(|elem| winning_numbers.contains(elem)))
-            || self
-                .cols
-                .iter()
-                .any(|col| col.iter().all(|elem| winning_numbers.contains(elem)))
-    }
-
-    pub fn unmarked_numbers(&self, winning_numbers: &[i32]) -> Vec<i32> {
-        self.rows
-            .iter()
-            .flatten()
-            .copied()
-            .filter(|elem| !winning_numbers.contains(elem))
-            .collect()
-    }
+#[derive(Display, FromStr, Debug, Copy, Clone)]
+#[display("{start} -> {end}")]
+struct Vector {
+    pub start: Point,
+    pub end: Point,
 }
 
-use std::fmt;
+impl Vector {
+    pub fn points_touched(&self) -> Vec<(i32, i32)> {
+        let rx = if self.start.x < self.end.x {
+            self.start.x..=self.end.x
+        } else {
+            self.end.x..=self.start.x
+        };
 
-impl fmt::Display for Board {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for row in self.rows.iter() {
-            write!(f, "{:?}\n", row).unwrap();
-        }
-        Ok(())
+        let ry = if self.start.y < self.end.y {
+            self.start.y..=self.end.y
+        } else {
+            self.end.y..=self.start.y
+        };
+
+        rx.cartesian_product(ry).collect()
     }
 }
 
 fn main() {
-    let input = fs::read_to_string("resources/day4.txt")
+    let input = fs::read_to_string("resources/day5.txt")
         .unwrap()
-        .split("\n\n")
-        .map(|s| String::from(s))
-        .collect::<Vec<String>>();
+        .split("\n")
+        .filter_map(|str| Vector::from_str(str).ok())
+        .filter(|vec| vec.start.x == vec.end.x || vec.start.y == vec.end.y)
+        .collect::<Vec<Vector>>();
 
-    let numbers: Vec<i32> = input[0]
-        .split(',')
-        .map(|c| c.parse::<i32>().unwrap())
-        .collect();
+    let points = input
+        .into_iter()
+        .map(|vector| vector.points_touched())
+        .flatten()
+        .counts()
+        .into_iter()
+        .filter(|&(_, freq)| freq > 1)
+        .count();
 
-    let mut boards: Vec<Board> = input
-        .iter()
-        .skip(1)
-        .map(|sg| {
-            Board::new(
-                sg.split('\n')
-                    .map(|row| {
-                        row.split_whitespace()
-                            .map(|c| c.parse::<i32>().unwrap())
-                            .collect::<Vec<i32>>()
-                    })
-                    .collect::<Vec<Vec<i32>>>(),
-            )
-        })
-        .collect::<Vec<Board>>();
-
-    println!("{}", numbers.len());
-
-    let mut winning_boards = Vec::new();
-
-    for idx in 1..=numbers.len() {
-        boards
-            .iter()
-            .filter(|b| b.check_for_bingo(&numbers[0..idx]))
-            .for_each(|b| {
-                println!("Board found on idx: {}", idx);
-                winning_boards.push((idx, b.clone()))
-            });
-        boards = boards
-            .iter()
-            .cloned()
-            .filter(|b| !b.check_for_bingo(&numbers[0..idx]))
-            .collect();
-    }
-
-    for (_, board) in winning_boards.iter() {
-        println!("{}", board);
-    }
-
-    if let Some((idx, board)) = winning_boards.last() {
-        let sum = board
-            .unmarked_numbers(&numbers[0..*idx])
-            .iter()
-            .sum::<i32>();
-        println!("{}", sum * numbers[idx - 1]);
-    }
+    println!("{:?}", points);
 }
