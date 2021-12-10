3f90af35a8cd205f6c64716dd099e85799e34b62
Tom Goring <mail@tomgoring.co.uk>
2021-12-10 18:43:13 +0000

    D5 p2

--- src/main.rs
+++ src/main.rs
@@ -18,19 +18,24 @@ struct Vector {
 
 impl Vector {
     pub fn points_touched(&self) -> Vec<(i32, i32)> {
-        let rx = if self.start.x < self.end.x {
-            self.start.x..=self.end.x
+        // println!("Getting points of {}", &self);
+        let rx: Vec<i32> = if self.start.x < self.end.x {
+            (self.start.x..=self.end.x).collect()
         } else {
-            self.end.x..=self.start.x
+            (self.end.x..=self.start.x).rev().collect()
         };
 
-        let ry = if self.start.y < self.end.y {
-            self.start.y..=self.end.y
+        let ry: Vec<i32> = if self.start.y < self.end.y {
+            (self.start.y..=self.end.y).collect()
         } else {
-            self.end.y..=self.start.y
+            (self.end.y..=self.start.y).rev().collect()
         };
 
-        rx.cartesian_product(ry).collect()
+        if (self.end.x - self.start.x).abs() == (self.end.y - self.start.y).abs() {
+            rx.into_iter().zip(ry).collect()
+        } else {
+            rx.into_iter().cartesian_product(ry).collect()
+        }
     }
 }
 
@@ -39,17 +44,30 @@ fn main() {
         .unwrap()
         .split("\n")
         .filter_map(|str| Vector::from_str(str).ok())
-        .filter(|vec| vec.start.x == vec.end.x || vec.start.y == vec.end.y)
+        // .inspect(|vec| println!("{}", vec))
         .collect::<Vec<Vector>>();
 
     let points = input
+        .clone()
         .into_iter()
         .map(|vector| vector.points_touched())
         .flatten()
+        // .inspect(|point| println!("{:?}", point))
         .counts()
         .into_iter()
         .filter(|&(_, freq)| freq > 1)
         .count();
 
-    println!("{:?}", points);
+    println!("{}", points);
+
+    // for y in 0..input.len() as i32 {
+    //     for x in 0..input.len() as i32 {
+    //         if let Some(count) = points.get(&(x, y)) {
+    //             print!("{} ", count);
+    //         } else {
+    //             print!(". ");
+    //         }
+    //     }
+    //     println!();
+    // }
 }
