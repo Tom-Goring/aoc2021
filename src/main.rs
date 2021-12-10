3364508de816193da27d78cc49a4543910629cc4
Tom Goring <mail@tomgoring.co.uk>
2021-12-10 19:52:22 +0000

    D6 p1

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,24 @@
-use itertools::Itertools;
-use parse_display::{Display, FromStr};
-use std::{fs, str::FromStr};
-
-#[derive(Display, FromStr, Debug, Copy, Clone)]
-#[display("{x},{y}")]
-struct Point {
-    x: i32,
-    y: i32,
-}
-
-#[derive(Display, FromStr, Debug, Copy, Clone)]
-#[display("{start} -> {end}")]
-struct Vector {
-    pub start: Point,
-    pub end: Point,
-}
-
-impl Vector {
-    pub fn points_touched(&self) -> Vec<(i32, i32)> {
-        // println!("Getting points of {}", &self);
-        let rx: Vec<i32> = if self.start.x < self.end.x {
-            (self.start.x..=self.end.x).collect()
-        } else {
-            (self.end.x..=self.start.x).rev().collect()
-        };
-
-        let ry: Vec<i32> = if self.start.y < self.end.y {
-            (self.start.y..=self.end.y).collect()
-        } else {
-            (self.end.y..=self.start.y).rev().collect()
-        };
-
-        if (self.end.x - self.start.x).abs() == (self.end.y - self.start.y).abs() {
-            rx.into_iter().zip(ry).collect()
-        } else {
-            rx.into_iter().cartesian_product(ry).collect()
-        }
-    }
-}
+use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("resources/day5.txt")
-        .unwrap()
-        .split("\n")
-        .filter_map(|str| Vector::from_str(str).ok())
-        // .inspect(|vec| println!("{}", vec))
-        .collect::<Vec<Vector>>();
+    let input = fs::read_to_string("./resources/day6.txt")
+        .expect("File not found")
+        .split(',')
+        .filter_map(|s| s.parse::<usize>().ok())
+        .fold([0; 9], |mut fish_counts, count| {
+            fish_counts[count] += 1;
+            fish_counts
+        });
 
-    let points = input
-        .clone()
+    let fishes = (0..80)
         .into_iter()
-        .map(|vector| vector.points_touched())
-        .flatten()
-        // .inspect(|point| println!("{:?}", point))
-        .counts()
-        .into_iter()
-        .filter(|&(_, freq)| freq > 1)
-        .count();
+        .fold(input, |mut input, _| {
+            input.rotate_left(1);
+            input[6] += input[8];
+            input
+        })
+        .iter()
+        .sum::<usize>();
 
-    println!("{}", points);
-
-    // for y in 0..input.len() as i32 {
-    //     for x in 0..input.len() as i32 {
-    //         if let Some(count) = points.get(&(x, y)) {
-    //             print!("{} ", count);
-    //         } else {
-    //             print!(". ");
-    //         }
-    //     }
-    //     println!();
-    // }
+    println!("{}", fishes);
 }
