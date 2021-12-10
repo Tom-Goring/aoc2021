973224e033a2c86b1998bbb2e6d0b9ae3784fa55
Tom Goring <mail@tomgoring.co.uk>
2021-12-10 21:43:08 +0000

    D7 p2

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
+use std::cmp;
 use std::fs;
-
-use itertools::Itertools;
 
 fn main() {
     let input = fs::read_to_string("./resources/day7.txt")
         .expect("File not found")
         .split(',')
         .map(|s| s.parse::<i32>().unwrap())
-        .sorted()
         .collect::<Vec<i32>>();
 
-    let median = input[input.len() / 2];
+    let min_fuel = (0..*input.iter().max().unwrap()).fold(i32::MAX, |min, end| {
+        cmp::min(
+            min,
+            input.iter().fold(0, |fuel, start| {
+                fuel + ((end - start).abs().pow(2) + (end - start).abs()) / 2
+            }),
+        )
+    });
 
-    let fuel = input
-        .iter()
-        .sorted()
-        .fold(0, |fuel, pos| fuel + (pos - median).abs());
-
-    println!("{}", fuel);
+    println!("{}", min_fuel);
 }
